5571dc5dc6aff7c88ff2fb6349a87563b2c3effe
JasonTongg <aboytong123@gmail.com>
2025-11-21 16:22:53 +0700

    liquidity list and remove liquidity page done + responsive

--- pages/v2/pools/index.js
+++ pages/v2/pools/index.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useLayoutEffect } from "react";
 import {
 	useWriteContract,
 	useAccount,
@@ -21,8 +21,11 @@ import {
 	Slider,
 } from "@mui/material";
 import { setNavbarActive } from "../../../store/data";
-
 import { fetchListPositions } from "../../../store/data";
+import { formatUnits } from "viem";
+import { IoMdClose } from "react-icons/io";
+import { toast } from "react-toastify";
+import { FaWaterLadder } from "react-icons/fa6";
 
 const erc20Abi = [
 	{
@@ -81,6 +84,8 @@ export default function RemoveLiquidityWithPositions() {
 	const { address } = useAccount();
 	const contractAddress = process.env.NEXT_PUBLIC_SWAP_CONTRACT;
 	const dispatch = useDispatch();
+	const spanRef = useRef(null);
+	const [width, setWidth] = useState(20);
 
 	const { data: apiResult, loading } = useSelector(
 		(state) => state.data.listPositions
@@ -89,10 +94,9 @@ export default function RemoveLiquidityWithPositions() {
 
 	const [openModal, setOpenModal] = useState(false);
 	const [selectedPos, setSelectedPos] = useState(null);
-	const [txResult, setTxResult] = useState(null);
 	const [liquidityAmount, setLiquidityAmount] = useState(0n);
-	const [statusMessage, setStatusMessage] = useState("");
-	const [percentage, setPercentage] = useState(100);
+	const [percentage, setPercentage] = useState(0);
+	const [poolType, setPoolType] = useState("All Pools");
 
 	const {
 		data: approveTxHash,
@@ -134,9 +138,7 @@ export default function RemoveLiquidityWithPositions() {
 
 	useEffect(() => {
 		if (isApprovalSuccess) {
-			setStatusMessage(
-				"Approval successful! Proceeding to remove liquidity..."
-			);
+			toast.success("Approval successful! Proceeding to remove liquidity...");
 			refetchAllowance();
 			executeRemoveLiquidity();
 		}
@@ -144,8 +146,7 @@ export default function RemoveLiquidityWithPositions() {
 
 	useEffect(() => {
 		if (isRemovalSuccess) {
-			setStatusMessage("Liquidity removed successfully! ✅");
-			setTxResult({ step: "Remove Confirmed", hash: removeTxHash });
+			toast.success("Liquidity removed successfully!");
 			if (address) {
 				dispatch(fetchListPositions({ address }));
 			}
@@ -156,24 +157,30 @@ export default function RemoveLiquidityWithPositions() {
 		dispatch(setNavbarActive("dashboard"));
 	}, []);
 
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (spanRef.current) {
+				setWidth(spanRef.current.offsetWidth + 10);
+			}
+		}, 0);
+
+		return () => clearTimeout(timer);
+	}, [percentage, openModal]);
+
 	function openRemoveModal(pos) {
 		setSelectedPos(pos);
-		setTxResult(null);
-		setStatusMessage("");
-		setLiquidityAmount(BigInt(pos.v2Pair.liquidity));
 		setPercentage(100);
+		setLiquidityAmount(BigInt(pos.v2Pair.liquidity));
 		setOpenModal(true);
 	}
 
 	function closeModal() {
 		setOpenModal(false);
 		setSelectedPos(null);
-		setTxResult(null);
-		setStatusMessage("");
 		setLiquidityAmount(0n);
-		setPercentage(100);
 		resetApprove();
 		resetRemove();
+		setPercentage(100);
 	}
 
 	const liquidityToRemove = (liquidityAmount * BigInt(percentage)) / 100n;
@@ -181,33 +188,25 @@ export default function RemoveLiquidityWithPositions() {
 	async function handleConfirm() {
 		if (!selectedPos || !address || !contractAddress) return;
 
-		setStatusMessage("");
-		setTxResult(null);
-
 		try {
 			if (currentAllowance < liquidityToRemove) {
-				setStatusMessage(
-					"Please approve spending your LP tokens in your wallet..."
-				);
+				toast.info("Approving...");
 				const tx = await approveAsync({
 					address: selectedPos.v2Pair.liquidityToken.address,
 					abi: erc20Abi,
 					functionName: "approve",
 					args: [contractAddress, liquidityToRemove],
 				});
-				setStatusMessage(
-					"Approval transaction sent. Waiting for confirmation..."
-				);
-				setTxResult({ step: "Approval Sent", hash: tx });
+				toast.info("Approval transaction sent. Waiting for confirmation...");
 			} else {
-				setStatusMessage(
+				toast.success(
 					"Allowance confirmed. Please confirm removal in your wallet..."
 				);
 				await executeRemoveLiquidity();
 			}
 		} catch (err) {
 			console.error(err);
-			setStatusMessage(`Error: ${err.message}`);
+			toast.error("Approval Failed...");
 		}
 	}
 
@@ -240,11 +239,10 @@ export default function RemoveLiquidityWithPositions() {
 					_,
 				});
 			}
-			setStatusMessage("Remove transaction sent! Waiting for confirmation...");
-			setTxResult({ step: "Remove Sent", hash: tx });
+			toast.success("Remove transaction sent! Waiting for confirmation...");
 		} catch (err) {
 			console.error(err);
-			setStatusMessage(`Error: ${err.message}`);
+			toast.success("Remove Liquidity Failed...");
 		}
 	}
 
@@ -260,68 +258,289 @@ export default function RemoveLiquidityWithPositions() {
 	else if (isWaitingForApproval) buttonText = "Approving...";
 	else if (isRemoving) buttonText = "Check Wallet (Remove)...";
 	else if (isWaitingForRemoval) buttonText = "Removing...";
-	else if (isRemovalSuccess) buttonText = "Done ✅";
+	else if (isRemovalSuccess) buttonText = "Done";
 	else if (needsApproval) buttonText = "Approve & Remove";
+
+	const getVersion = (resp) => {
+		if (resp === "PROTOCOL_VERSION_V2") {
+			return "V2";
+		} else if (resp === "PROTOCOL_VERSION_V3") {
+			return "V3";
+		} else if (resp === "PROTOCOL_VERSION_V4") {
+			return "V4";
+		}
+	};
+
+	function formatNumber(num) {
+		num = Number(num);
+
+		if (num >= 1e12) return (num / 1e12).toFixed(2) + "T";
+		if (num >= 1e9) return (num / 1e9).toFixed(2) + "B";
+		if (num >= 1e6) return (num / 1e6).toFixed(2) + "M";
+		if (num >= 1e3) return (num / 1e3).toFixed(2) + "K";
+
+		return num.toString();
+	}
+
+	const filterVersion = () => {
+		let data = positions;
+
+		if (poolType.toLowerCase() === "all pools") {
+			return data;
+		} else if (poolType.toLowerCase() === "v2") {
+			return data.filter(
+				(item) => item.protocolVersion === "PROTOCOL_VERSION_V2"
+			);
+		} else if (poolType.toLowerCase() === "v3") {
+			return data.filter(
+				(item) => item.protocolVersion === "PROTOCOL_VERSION_V3"
+			);
+		} else if (poolType.toLowerCase() === "v4") {
+			return data.filter(
+				(item) => item.protocolVersion === "PROTOCOL_VERSION_V4"
+			);
+		}
+	};
 
 	return (
 		<div
-			className='p-10 max-w-2xl mx-auto space-y-6 mt-[4.5rem] flex flex-col items-center justify-center w-full'
+			className='p-10 max-w-5xl space-y-6 mt-[4.5rem] flex flex-col items-center justify-start w-full'
 			style={{ minHeight: "calc(100vh - 200px)" }}
 		>
-			<h1 className='text-2xl font-bold'>My Liquidity Positions</h1>
+			<div className='flex flex-col items-start justify-center gap-2 w-full'>
+				<h1 className='text-3xl font-bold'>Dashboard</h1>
+				<p className='text-lg pb-[1rem]'>
+					Manage your liquidity positions, track real-time insights, and
+					optimize earnings with ease.
+				</p>
+				<div className='flex items-center justify-center sm:justify-start gap-4 flex-wrap'>
+					<button
+						className='py-2 px-6 border-[1px] border-gray-300 flex items-center justify-center rounded-[7px]'
+						style={
+							poolType.toLowerCase() === "all pools"
+								? { background: "rgba(39,117,202,0.25)", color: "#2775CA" }
+								: { background: "transparent", color: "black" }
+						}
+						onClick={() => setPoolType("all pools")}
+					>
+						All Pools
+					</button>
+					<button
+						className='py-2 px-6 border-[1px] border-gray-300 flex items-center justify-center rounded-[7px]'
+						style={
+							poolType.toLowerCase() === "v2"
+								? { background: "rgba(39,117,202,0.25)", color: "#2775CA" }
+								: { background: "transparent", color: "black" }
+						}
+						onClick={() => setPoolType("v2")}
+					>
+						V2
+					</button>
+					<button
+						className='py-2 px-6 border-[1px] border-gray-300 flex items-center justify-center rounded-[7px] '
+						style={
+							poolType.toLowerCase() === "v3"
+								? { background: "rgba(39,117,202,0.25)", color: "#2775CA" }
+								: { background: "transparent", color: "black" }
+						}
+						onClick={() => setPoolType("v3")}
+					>
+						V3
+					</button>
+					<button
+						className='py-2 px-6 border-[1px] border-gray-300 flex items-center justify-center rounded-[7px] '
+						style={
+							poolType.toLowerCase() === "v4"
+								? { background: "rgba(39,117,202,0.25)", color: "#2775CA" }
+								: { background: "transparent", color: "black" }
+						}
+						onClick={() => setPoolType("v4")}
+					>
+						V4
+					</button>
+				</div>
+			</div>
 			{loading ? (
 				<div className='flex justify-center p-6'>
 					<CircularProgress />
 				</div>
-			) : (
-				<div className='space-y-4'>
-					{positions.map((pos, i) => (
+			) : filterVersion().length > 0 ? (
+				<div className='grid w-full gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+					{filterVersion().map((pos, i) => (
 						<div
 							key={i}
-							className='p-4 border rounded-xl flex justify-between items-center'
+							className='p-4 border rounded-xl flex justify-center flex-col items-center gap-2'
 						>
-							<div>
-								<p className='font-semibold'>
-									{pos.v2Pair.token0.symbol}/ {pos.v2Pair.token1.symbol}
+							<div className='flex items-center justify-between w-full'>
+								<p className='text-2xl'>
+									{pos?.v2Pair?.token0?.symbol ||
+										pos?.v3Position?.token0?.symbol ||
+										pos?.v4Position?.poolPosition?.token0?.symbol}
+									/
+									{pos?.v2Pair?.token1?.symbol ||
+										pos?.v3Position?.token1?.symbol ||
+										pos?.v4Position?.poolPosition?.token1?.symbol}
 								</p>
-								<p className='text-sm text-gray-600'>
-									LP: {pos.v2Pair.liquidity}
-								</p>
+								<div className='flex items-center justify-start gap-1'>
+									<p className='bg-[rgba(39,117,202,0.25)] text-[#2775CA] px-2 py-1 rounded-[5px] text-xs'>
+										{getVersion(pos.protocolVersion)}
+									</p>
+									<p className='bg-[rgba(39,117,202,0.25)] text-[#2775CA] px-2 py-1 rounded-[5px] text-xs'>
+										0.3%
+									</p>
+								</div>
 							</div>
-							<Button variant='contained' onClick={() => openRemoveModal(pos)}>
-								Remove
-							</Button>
+							{getVersion(pos.protocolVersion) === "V2" ? (
+								<div className='bg-[rgba(39,117,202,0.25)] flex flex-col items-center justify-center gap-2 p-2 w-full rounded-[5px] my-2'>
+									<div className='flex items-center justify-between w-full'>
+										<p>My Position</p>
+										<p>{formatNumber(pos?.v2Pair?.liquidity)}</p>
+									</div>
+									<div className='flex items-center justify-between w-full'>
+										<p>Total Supply</p>
+										<p>{formatNumber(pos?.v2Pair?.totalSupply)}</p>
+									</div>
+								</div>
+							) : getVersion(pos.protocolVersion) === "V4" ? (
+								<div className='bg-[rgba(39,117,202,0.25)] flex flex-col items-center justify-center gap-2 p-2 w-full rounded-[5px] my-2'>
+									<div className='flex items-center justify-between w-full'>
+										<p>
+											{pos?.v4Position?.poolPosition?.token0?.symbol} Amount
+										</p>
+										<p>
+											{Number(
+												formatUnits(pos?.v4Position?.poolPosition?.amount0, 18)
+											).toFixed(6)}
+										</p>
+									</div>
+									<div className='flex items-center justify-between w-full'>
+										<p>
+											{pos?.v4Position?.poolPosition?.token1?.symbol} Amount
+										</p>
+										<p>
+											{Number(
+												formatUnits(pos?.v4Position?.poolPosition?.amount1, 18)
+											).toFixed(6)}
+										</p>
+									</div>
+								</div>
+							) : (
+								<div className='bg-[rgba(39,117,202,0.25)] flex flex-col items-center justify-center gap-2 p-2 w-full rounded-[5px] my-2'>
+									<div className='flex items-center justify-between w-full'>
+										<p>{pos?.v3Position?.token0?.symbol} Amount</p>
+										<p>
+											{Number(
+												formatUnits(pos?.v3Position?.amount0, 18)
+											).toFixed(6)}
+										</p>
+									</div>
+									<div className='flex items-center justify-between w-full'>
+										<p>{pos?.v3Position?.token1?.symbol} Amount</p>
+										<p>
+											{Number(
+												formatUnits(pos?.v3Position?.amount1, 18)
+											).toFixed(6)}
+										</p>
+									</div>
+								</div>
+							)}
+							<button
+								className='p-2 text-center bg-[rgba(39,117,202,0.25)] text-[#2775CA] w-full rounded-[5px] disabled:opacity-30'
+								onClick={() => openRemoveModal(pos)}
+								disabled={
+									getVersion(pos.protocolVersion) === "V3" ||
+									getVersion(pos.protocolVersion) === "V4"
+								}
+							>
+								Remove Liquidity
+							</button>
 						</div>
 					))}
 				</div>
+			) : (
+				<div className='flex flex-col items-center justify-center gap-2 border-[1px] rounded-[10px] border-gray-300 w-full min-h-[450px]'>
+					<div className='bg-[rgba(39,117,202,0.25)] text-[#2775CA] w-[50px] h-[50px] rounded-[5px] flex items-center justify-center'>
+						<FaWaterLadder className='text-[1.7rem]' />
+					</div>
+					<h2 className='font-semibold text-[1.15rem]'>
+						No Liquidity Positions Found
+					</h2>
+					<p className='text-gray-600'>
+						Add liquidity to a pool and view your positions here
+					</p>
+					<button className='bg-[rgba(39,117,202,0.25)] text-[#2775CA] py-[6px] px-[30px] rounded-[5px]'>
+						Add Liquidity
+					</button>
+				</div>
 			)}
-			<Dialog open={openModal} onClose={closeModal} fullWidth maxWidth='sm'>
-				<DialogTitle>Remove Liquidity</DialogTitle>
-				<DialogContent>
+			<Dialog
+				open={openModal}
+				onClose={closeModal}
+				fullWidth
+				PaperProps={{
+					sx: {
+						width: "450px",
+						maxWidth: "90vw",
+						borderRadius: "12px",
+					},
+				}}
+			>
+				<div className='flex items-center justify-between gap-3 w-full px-[1rem] bg-[#FFFFE3]'>
+					<IoMdClose
+						onClick={closeModal}
+						className='cursor-pointer text-2xl text-[1.6rem]'
+					/>
+					<DialogTitle>Remove Liquidity</DialogTitle>
+					<IoMdClose className='cursor-pointer opacity-0' />
+				</div>
+				<DialogContent sx={{ background: "#FFFFE3" }}>
 					{selectedPos && (
-						<div className='space-y-2'>
-							<p>
-								Pair:
-								<b>
-									{selectedPos.v2Pair.token0.symbol} /{" "}
-									{selectedPos.v2Pair.token1.symbol}
-								</b>
-							</p>
-							<p>Total Liquidity: {selectedPos.v2Pair.liquidity}</p>
+						<div className='flex flex-col items-start justify-center gap-1'>
+							<div className='flex items-center justify-start gap-2'>
+								<p className='text-2xl'>
+									{selectedPos?.v2Pair?.token0?.symbol ||
+										selectedPos?.v3Position?.token0?.symbol ||
+										selectedPos?.v4Position?.poolPosition?.token0?.symbol}
+									/
+									{selectedPos?.v2Pair?.token1?.symbol ||
+										selectedPos?.v3Position?.token1?.symbol ||
+										selectedPos?.v4Position?.poolPosition?.token1?.symbol}
+								</p>
+								<p className='bg-[rgba(39,117,202,0.25)] text-[#2775CA] px-2 py-1 rounded-[5px] text-xs'>
+									{getVersion(selectedPos.protocolVersion)}
+								</p>
+							</div>
+							<div className='flex items-center justify-start gap-2'>
+								<span class='relative flex size-3'>
+									<span class='absolute inline-flex h-full w-full animate-ping rounded-full bg-[#FF007A] opacity-75'></span>
+									<span class='relative inline-flex size-3 rounded-full bg-[#FF007A]'></span>
+								</span>
+								<p className='text-[#FF007A]'>In range</p>
+							</div>
 						</div>
 					)}
 					<Box className='mt-6'>
-						<Typography gutterBottom>
-							Amount to Remove: {percentage}%
-						</Typography>
-						<Slider
-							aria-label='Percentage'
-							value={percentage}
-							onChange={(e, newValue) => setPercentage(newValue)}
-							step={1}
-							min={1}
-							max={100}
-						/>
+						<Typography gutterBottom>Withdrawal amount</Typography>
+						<div className='relative inline-flex items-center justify-center w-full'>
+							<span
+								ref={spanRef}
+								className='absolute invisible whitespace-pre text-7xl font-bold'
+							>
+								{percentage || "0"}
+							</span>
+							<input
+								type='text'
+								inputmode='numeric'
+								step={1}
+								min={0}
+								max={100}
+								value={percentage}
+								onChange={(e) => setPercentage(e.target.value)}
+								className='border-none bg-transparent text-7xl font-bold outline-none'
+								style={{ width }}
+							/>
+							<span className='text-7xl font-bold translate-x-[-5px]'>%</span>
+						</div>
 						<Stack
 							direction='row'
 							spacing={1}
@@ -359,28 +578,9 @@ export default function RemoveLiquidityWithPositions() {
 							</Button>
 						</Stack>
 					</Box>
-					{statusMessage && (
-						<Typography variant='body2' className='text-center mt-4'>
-							{statusMessage}
-						</Typography>
-					)}
-					{isBusy && !isRemovalSuccess && (
-						<div className='flex justify-center mt-4'>
-							<CircularProgress />
-						</div>
-					)}
-					{txResult && (
-						<pre className='bg-gray-100 p-3 rounded mt-4 text-sm whitespace-pre-wrap'>
-							{JSON.stringify(txResult, null, 2)}
-						</pre>
-					)}
 				</DialogContent>
-				<DialogActions>
-					<Button onClick={closeModal}>
-						{isRemovalSuccess ? "Close" : "Cancel"}
-					</Button>
-					<Button
-						variant='contained'
+				<DialogActions sx={{ background: "#FFFFE3", padding: "0px" }}>
+					<button
 						onClick={handleConfirm}
 						disabled={
 							isBusy ||
@@ -388,9 +588,10 @@ export default function RemoveLiquidityWithPositions() {
 							isRemovalSuccess ||
 							percentage === 0
 						}
+						className='w-full bg-[rgba(39,117,202,0.25)] text-[#2775CA] py-2 px-4 text-center font-semibold'
 					>
 						{buttonText}
-					</Button>
+					</button>
 				</DialogActions>
 			</Dialog>
 		</div>
